57495536ce45d382b32149cfaff4499a7de36c09
Charles Lambert <143451737+clambert2@users.noreply.github.com>
2025-11-06 19:43:14 -0800

    hw7

---
+++ homework_7/hw_11_18_19.m
@@ -0,0 +1,273 @@
+function [t,VAR,Output] = hw_11_18_19
+%===========================================================================
+% File: hw_11_18_19.m created Nov 06 2025 by MotionGenesis 6.5.
+% Portions copyright (c) 2009-2025 Motion Genesis LLC.  Rights reserved.
+% MotionGenesis Student Licensee: Charlie Lambert. (until September 2028).
+% This MotionGenesis Student license is granted the right to use this code
+% only until September 2028, only for legal student-academic (non-professional) purposes,
+% and limited to their coursework completion at their accredited school.
+% All use of this code is specifically limited to this student licensee.
+% No rights are extended for other purposes or to anyone but the licensee.
+% This copyright notice must appear in all uses of this code.
+%===========================================================================
+% The software is provided "as is", without warranty of any kind, express or    
+% implied, including but not limited to the warranties of merchantability or    
+% fitness for a particular purpose. In no event shall the authors, contributors,
+% or copyright holders be liable for any claim, damages or other liability,     
+% whether in an action of contract, tort, or otherwise, arising from, out of, or
+% in connection with the software or the use or other dealings in the software. 
+%===========================================================================
+eventDetectedByIntegratorTerminate1OrContinue0 = [];
+phiDDt=0; qCDDt=0; thetaDDt=0;
+
+
+%-------------------------------+--------------------------+-------------------+-----------------
+% Quantity                      | Value                    | Units             | Description
+%-------------------------------|--------------------------|-------------------|-----------------
+g                               =  9.8;                    % m/s^2               Constant
+L                               =  0.2;                    % m                   Constant
+r                               =  0.2;                    % m                   Constant
+
+phi                             =  20;                     % degrees             Initial Value
+qC                              =  0;                      % UNITS               Initial Value
+theta                           =  0;                      % degrees             Initial Value
+phiDt                           =  0;                      % UNITS               Initial Value
+qCDt                            =  300;                    % rpm                 Initial Value
+thetaDt                         =  0;                      % UNITS               Initial Value
+
+tInitial                        =  0.0;                    % second              Initial Time
+tFinal                          =  4;                      % second              Final Time
+tStep                           =  0.05;                   % second              Integration Step
+printIntScreen                  =  1;                      % 0 or +integer       0 is NO screen output
+printIntFile                    =  1;                      % 0 or +integer       0 is NO file   output
+absError                        =  1.0E-05;                %                     Absolute Error
+relError                        =  1.0E-08;                %                     Relative Error
+%-------------------------------+--------------------------+-------------------+-----------------
+
+% Unit conversions.  UnitSystem: kg, meter, second.
+DEGtoRAD = pi / 180.0;
+RADtoDEG = 180.0 / pi;
+phi = phi * DEGtoRAD;                                      %  Converted from degrees 
+theta = theta * DEGtoRAD;                                  %  Converted from degrees 
+qCDt = qCDt * 0.1047197551196598;                          %  Converted from rpm 
+
+VAR = SetMatrixFromNamedQuantities;
+[t,VAR,Output] = IntegrateForwardOrBackward( tInitial, tFinal, tStep, absError, relError, VAR, printIntScreen, printIntFile );
+OutputToScreenOrFile( [], 0, 0 );   % Close output files.
+if( printIntFile ~= 0 ),  PlotOutputFiles;  end
+
+
+%===========================================================================
+function sys = mdlDerivatives( t, VAR, uSimulink )
+%===========================================================================
+SetNamedQuantitiesFromMatrix( VAR );
+thetaDDt = -2*phiDt*(r^2*qCDt+4*L^2*cos(phi)*thetaDt)/((r^2+4*L^2)*sin(phi));
+phiDDt = sin(phi)*(4*g*L+4*L^2*cos(phi)*thetaDt^2+r^2*thetaDt*(2*qCDt-cos(phi)*thetaDt))/(r^2+4*L^2);
+qCDDt = -phiDt*((2*r^2+(4*L^2-r^2)*sin(phi)^2)*thetaDt+2*cos(phi)*(r^2*qCDt+4*L^2*cos(phi)*thetaDt-r^2*cos(phi)*thetaDt))/((r^2+4*L^2)*  ...
+sin(phi));
+
+sys = transpose( SetMatrixOfDerivativesPriorToIntegrationStep );
+end
+
+
+
+%===========================================================================
+function VAR = SetMatrixFromNamedQuantities
+%===========================================================================
+VAR = zeros( 1, 6 );
+VAR(1) = phi;
+VAR(2) = qC;
+VAR(3) = theta;
+VAR(4) = phiDt;
+VAR(5) = qCDt;
+VAR(6) = thetaDt;
+end
+
+
+%===========================================================================
+function SetNamedQuantitiesFromMatrix( VAR )
+%===========================================================================
+phi = VAR(1);
+qC = VAR(2);
+theta = VAR(3);
+phiDt = VAR(4);
+qCDt = VAR(5);
+thetaDt = VAR(6);
+end
+
+
+%===========================================================================
+function VARp = SetMatrixOfDerivativesPriorToIntegrationStep
+%===========================================================================
+VARp = zeros( 1, 6 );
+VARp(1) = phiDt;
+VARp(2) = qCDt;
+VARp(3) = thetaDt;
+VARp(4) = phiDDt;
+VARp(5) = qCDDt;
+VARp(6) = thetaDDt;
+end
+
+
+
+%===========================================================================
+function Output = mdlOutputs( t, VAR, uSimulink )
+%===========================================================================
+Output = zeros( 1, 4 );
+Output(1) = t;
+Output(2) = theta*RADtoDEG;
+
+Output(3) = t;
+Output(4) = phi*RADtoDEG;
+end
+
+
+%===========================================================================
+function OutputToScreenOrFile( Output, shouldPrintToScreen, shouldPrintToFile )
+%===========================================================================
+persistent FileIdentifier hasHeaderInformationBeenWritten;
+
+if( isempty(Output) ),
+   if( ~isempty(FileIdentifier) ),
+      for( i = 1 : 2 ),  fclose( FileIdentifier(i) );  end
+      clear FileIdentifier;
+      fprintf( 1, '\n Output is in the files hw_11_18_19.i  (i=1,2)\n\n' );
+   end
+   clear hasHeaderInformationBeenWritten;
+   return;
+end
+
+if( isempty(hasHeaderInformationBeenWritten) ),
+   if( shouldPrintToScreen ),
+      fprintf( 1,                '%%       t            theta\n' );
+      fprintf( 1,                '%%   (second)       (degrees)\n\n' );
+   end
+   if( shouldPrintToFile && isempty(FileIdentifier) ),
+      FileIdentifier = zeros( 1, 2 );
+      FileIdentifier(1) = fopen('hw_11_18_19.1', 'wt');   if( FileIdentifier(1) == -1 ), error('Error: unable to open file hw_11_18_19.1'); end
+      fprintf(FileIdentifier(1), '%% FILE: hw_11_18_19.1\n%%\n' );
+      fprintf(FileIdentifier(1), '%%       t            theta\n' );
+      fprintf(FileIdentifier(1), '%%   (second)       (degrees)\n\n' );
+      FileIdentifier(2) = fopen('hw_11_18_19.2', 'wt');   if( FileIdentifier(2) == -1 ), error('Error: unable to open file hw_11_18_19.2'); end
+      fprintf(FileIdentifier(2), '%% FILE: hw_11_18_19.2\n%%\n' );
+      fprintf(FileIdentifier(2), '%%       t             phi\n' );
+      fprintf(FileIdentifier(2), '%%   (second)       (degrees)\n\n' );
+   end
+   hasHeaderInformationBeenWritten = 1;
+end
+
+if( shouldPrintToScreen ), WriteNumericalData( 1,                 Output(1:2) );  end
+if( shouldPrintToFile ),   WriteNumericalData( FileIdentifier(1), Output(1:2) );  end
+if( shouldPrintToFile ),   WriteNumericalData( FileIdentifier(2), Output(3:4) );  end
+end
+
+
+%===========================================================================
+function WriteNumericalData( fileIdentifier, Output )
+%===========================================================================
+numberOfOutputQuantities = length( Output );
+if( numberOfOutputQuantities > 0 ),
+   for( i = 1 : numberOfOutputQuantities ),
+      fprintf( fileIdentifier, ' %- 14.6E', Output(i) );
+   end
+   fprintf( fileIdentifier, '\n' );
+end
+end
+
+
+
+%===========================================================================
+function PlotOutputFiles
+%===========================================================================
+figure;
+data = load( 'hw_11_18_19.1' ); 
+plot( data(:,1),data(:,2),'-b', 'LineWidth',3 );
+title('Precession: \theta^{\circ} versus t (sec)')
+xlabel('t (sec)');   ylabel('\theta^{\circ}');   % title('Some plot title');
+clear data;
+
+figure;
+data = load( 'hw_11_18_19.2' ); 
+plot( data(:,1),data(:,2),'-b', 'LineWidth',3 );
+title('Nutation: \phi^{\circ} versus t (sec)')
+xlabel('t (sec)');   ylabel('\phi^{\circ}');   % title('Some plot title');
+clear data;
+end
+
+
+
+%===========================================================================
+function [functionsToEvaluateForEvent, eventTerminatesIntegration1Otherwise0ToContinue, eventDirection_AscendingIs1_CrossingIs0_DescendingIsNegative1] = EventDetection( t, VAR, uSimulink )
+%===========================================================================
+% Detects when designated functions are zero or cross zero with positive or negative slope.
+% Step 1: Uncomment call to mdlDerivatives and mdlOutputs.
+% Step 2: Change functionsToEvaluateForEvent,                      e.g., change  []  to  [t - 5.67]  to stop at t = 5.67.
+% Step 3: Change eventTerminatesIntegration1Otherwise0ToContinue,  e.g., change  []  to  [1]  to stop integrating.
+% Step 4: Change eventDirection_AscendingIs1_CrossingIs0_DescendingIsNegative1,  e.g., change  []  to  [1].
+% Step 5: Possibly modify function EventDetectedByIntegrator (if eventTerminatesIntegration1Otherwise0ToContinue is 0).
+%---------------------------------------------------------------------------
+% mdlDerivatives( t, VAR, uSimulink );        % UNCOMMENT FOR EVENT HANDLING
+% mdlOutputs(     t, VAR, uSimulink );        % UNCOMMENT FOR EVENT HANDLING
+functionsToEvaluateForEvent = [];
+eventTerminatesIntegration1Otherwise0ToContinue = [];
+eventDirection_AscendingIs1_CrossingIs0_DescendingIsNegative1 = [];
+eventDetectedByIntegratorTerminate1OrContinue0 = eventTerminatesIntegration1Otherwise0ToContinue;
+end
+
+
+%===========================================================================
+function [isIntegrationFinished, VAR] = EventDetectedByIntegrator( t, VAR, nIndexOfEvents )
+%===========================================================================
+isIntegrationFinished = eventDetectedByIntegratorTerminate1OrContinue0( nIndexOfEvents );
+if( ~isIntegrationFinished ),
+   SetNamedQuantitiesFromMatrix( VAR );
+%  Put code here to modify how integration continues.
+   VAR = SetMatrixFromNamedQuantities;
+end
+end
+
+
+
+%===========================================================================
+function [t,VAR,Output] = IntegrateForwardOrBackward( tInitial, tFinal, tStep, absError, relError, VAR, printIntScreen, printIntFile )
+%===========================================================================
+OdeMatlabOptions = odeset( 'RelTol',relError, 'AbsTol',absError, 'MaxStep',tStep, 'Events',@EventDetection );
+t = tInitial;                 epsilonT = 0.001*tStep;                   tFinalMinusEpsilonT = tFinal - epsilonT;
+printCounterScreen = 0;       integrateForward = tFinal >= tInitial;    tAtEndOfIntegrationStep = t + tStep;
+printCounterFile   = 0;       isIntegrationFinished = 0;
+mdlDerivatives( t, VAR, 0 );
+while 1,
+   if( (integrateForward && t >= tFinalMinusEpsilonT) || (~integrateForward && t <= tFinalMinusEpsilonT) ), isIntegrationFinished = 1;  end
+   shouldPrintToScreen = printIntScreen && ( isIntegrationFinished || printCounterScreen <= 0.01 );
+   shouldPrintToFile   = printIntFile   && ( isIntegrationFinished || printCounterFile   <= 0.01 );
+   if( isIntegrationFinished || shouldPrintToScreen || shouldPrintToFile ),
+      Output = mdlOutputs( t, VAR, 0 );
+      OutputToScreenOrFile( Output, shouldPrintToScreen, shouldPrintToFile );
+      if( isIntegrationFinished ), break;  end
+      if( shouldPrintToScreen ), printCounterScreen = printIntScreen;  end
+      if( shouldPrintToFile ),   printCounterFile   = printIntFile;    end
+   end
+   [TimeOdeArray, VarOdeArray, timeEventOccurredInIntegrationStep, nStatesArraysAtEvent, nIndexOfEvents] = ode45( @mdlDerivatives, [t tAtEndOfIntegrationStep], VAR, OdeMatlabOptions, 0 );
+   if( isempty(timeEventOccurredInIntegrationStep) ),
+      lastIndex = length( TimeOdeArray );
+      t = TimeOdeArray( lastIndex );
+      VAR = VarOdeArray( lastIndex, : );
+      printCounterScreen = printCounterScreen - 1;
+      printCounterFile   = printCounterFile   - 1;
+      if( abs(tAtEndOfIntegrationStep - t) >= abs(epsilonT) ), warning('numerical integration failed'); break;  end
+      tAtEndOfIntegrationStep = t + tStep;
+      if( (integrateForward && tAtEndOfIntegrationStep > tFinal) || (~integrateForward && tAtEndOfIntegrationStep < tFinal) ) tAtEndOfIntegrationStep = tFinal;  end
+   else
+      t = timeEventOccurredInIntegrationStep( 1 );    % time  at firstEvent = 1 during this integration step.
+      VAR = nStatesArraysAtEvent( 1, : );             % state at firstEvent = 1 during this integration step.
+      printCounterScreen = 0;
+      printCounterFile   = 0;
+      [isIntegrationFinished, VAR] = EventDetectedByIntegrator( t, VAR, nIndexOfEvents(1) );
+   end
+end
+end
+
+
+%====================================
+end    % End of function hw_11_18_19
+%====================================
